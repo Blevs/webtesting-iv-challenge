0d05e58edc938c8aff74a64854344e6963e27667
Henry Blevins <heblevi@gmail.com>
2019-05-16 18:57:10 -0400

    Implement delete

--- smurfs/routes.js
+++ smurfs/routes.js
@@ -6,13 +6,21 @@ const router = express.Router();
 router.post('/', (req, res) => {
   const smurf = req.body;
   if (smurf && smurf.name && smurf.weight) {
-    console.log(smurf);
     Smurfs.insert(smurf)
       .then(smurf => res.status(201).json(smurf))
       .catch(() => res.status(500).json({message: 'Error inserting smurf'}));
   } else {
     res.status(400).json({message: 'Requires name and weight'});
   }
+});
+
+router.delete('/:id', (req, res) => {
+  const {id} = req.params;
+  Smurfs.remove(id)
+    .then(deleted => deleted
+          ? res.status(204).end()
+          : res.status(404).json({message: 'Smurf with id does not exist'}))
+    .catch(() => res.status(500).json({message: 'Error deleting smurf'}));
 });
 
 module.exports = router;
