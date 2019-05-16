c12a3e613be95e9aa60d25e120d05c70d3f5801f
Henry Blevins <heblevi@gmail.com>
2019-05-16 17:58:08 -0400

    Write smurfs model

---
+++ smurfs/model.js
@@ -0,0 +1,30 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+  get,
+  insert,
+  remove,
+  update,
+};
+
+function get(id) {
+  if (id) {
+    return db('smurfs').where({id});
+  } else {
+    return db('smurfs');
+  }
+}
+
+function insert(smurf) {
+  return db('smurfs').insert(smurf, 'id').then(([id]) => get(id));
+}
+
+function remove(id) {
+  return db('smurfs').where({id}).del();
+}
+
+function update(id, changes) {
+  return db('smurfs').where({id}).update(changes, 'id').then(([id]) => get(id));
+}
+
+
