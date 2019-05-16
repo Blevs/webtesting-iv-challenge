f78f6c129063a8f9e9915d4a1ac637e80c9be5cf
Henry Blevins <heblevi@gmail.com>
2019-05-16 18:50:30 -0400

    Write post route

--- smurfs/routes.js
+++ smurfs/routes.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const Smurfs = require('./model.js');
 
 const router = express.Router();
+
+router.post('/', (req, res) => {
+  const smurf = req.body;
+  if (smurf && smurf.name && smurf.weight) {
+    console.log(smurf);
+    Smurfs.insert(smurf)
+      .then(smurf => res.status(201).json(smurf))
+      .catch(() => res.status(500).json({message: 'Error inserting smurf'}));
+  } else {
+    res.status(400).json({message: 'Requires name and weight'});
+  }
+});
 
 module.exports = router;
