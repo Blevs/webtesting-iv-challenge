3f3b62798524f225471ee41c0e937d9c0df1ae78
Henry Blevins <heblevi@gmail.com>
2019-05-16 18:50:21 -0400

    Write post tests

--- smurfs/routes.spec.js
+++ smurfs/routes.spec.js
@@ -1,8 +1,31 @@
 const request = require('supertest');
 const server = require('../server.js');
+const db = require('../data/dbConfig.js');
 
 describe('smurfs routes', () => {
+  afterEach(async () => {
+    await db('smurfs').truncate();
+  });
+
   it('in test environment', () => {
     expect(process.env.DB_ENV).toBe('testing');
+  });
+
+  describe('post /', () => {
+    it('create new smurf', async () => {
+      const res = await request(server).post('/api/smurfs/').send({name: 'Henry', weight: 200});
+      expect(res.status).toBe(201);
+    });
+    it('post returns smurf', async () => {
+      const res = await request(server).post('/api/smurfs/').send({name: 'Henry', weight: 200});
+      expect(res.body.name).toBe('Henry');
+      expect(res.body.weight).toBe(200);
+    });
+    it('Error on insufficient info', async () => {
+      let res = await request(server).post('/api/smurfs/').send({weight: 200});
+      expect(res.status).toBe(400);
+      res = await request(server).post('/api/smurfs/').send({name: 'Test'});
+      expect(res.status).toBe(400);
+    });
   });
 });
